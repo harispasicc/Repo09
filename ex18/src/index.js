1f11c909513a2e7b90d45758788fd08aba45db33
Haris Pasic <harispasic@ymail.com>
2022-04-06 05:18:23 +0200

    For loops

--- ex18/src/index.js
+++ ex18/src/index.js
@@ -1,16 +1,27 @@
 function myForLoop1(){
+    var evenNumbers = [];
 for(i=0; i<9; i++){
     if(i % 2 === 0){
-        console.log(i)
+        evenNumbers.push(i);
     }  
+}return evenNumbers.join(",");
 }
+
+function myForLoop2(){
+    var evenInverseNumbers = [];
+for(i=0; i<9; i++){
+    if(i % 2 === 0){
+        evenInverseNumbers.push(i);
+    }  
+}return evenInverseNumbers.reverse().join(",");
 }
 
 
 
-myForLoop1();
+console.log(myForLoop1());
+console.log(myForLoop2());
 
 module.exports = {
     myForLoop1,
-    
+    myForLoop2
 };
